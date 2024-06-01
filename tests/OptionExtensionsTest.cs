0f3522f19aebb41cb79254375da1dfb344b78d98
Atkinstinkmoore <44531198+Atkinstinkmoore@users.noreply.github.com>
2024-06-01 20:33:41 +0200

    add unwrap function
    unwrap a collection of optional values

---
+++ tests/OptionExtensionsTest.cs
@@ -0,0 +1,36 @@
+﻿using Sork.Funk;
+
+namespace Sork.Funk.Tests;
+
+public class OptionExtensionsTest
+{
+    [Fact]
+    public void Unwrap_WithOnlyNone_ShouldBeEmpty()
+    {
+        new List<Option<int>> { Option<int>.None, Option<int>.None }.Unwrap().Should().BeEmpty();
+    }
+
+    [Fact]
+    public void Unwrap_WithOnlySome_ShouldNotBeEmpty()
+    {
+        new List<Option<int>> { Option<int>.Some(3), Option<int>.Some(2) }
+            .Unwrap()
+            .Should()
+            .NotBeEmpty().And
+            .HaveCount(2);
+    }
+
+    [Fact]
+    public void unwrap_WithMixed_ShouldHaveCorrectCount()
+    {
+        new List<Option<string>>
+            {
+                Option<string>.Some("test"),
+                Option<string>.None,
+                Option<string>.Some("next"),
+                Option<string>.Some("again"),
+                Option<string>.None
+            }.Unwrap()
+            .Should().NotBeEmpty().And.HaveCount(3);
+    }
+}
