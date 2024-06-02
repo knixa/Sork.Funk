cfe03a9df55be5666be110420404763d30168e18
Atkinstinkmoore <44531198+Atkinstinkmoore@users.noreply.github.com>
2024-06-02 08:21:29 +0200

    apply formatting rules

--- tests/OptionExtensionsTest.cs
+++ tests/OptionExtensionsTest.cs
@@ -5,24 +5,19 @@ namespace Sork.Funk.Tests;
 public class OptionExtensionsTest
 {
     [Fact]
-    public void Unwrap_WithOnlyNone_ShouldBeEmpty()
-    {
+    public void Unwrap_WithOnlyNone_ShouldBeEmpty() =>
         new List<Option<int>> { Option<int>.None, Option<int>.None }.Unwrap().Should().BeEmpty();
-    }
 
     [Fact]
-    public void Unwrap_WithOnlySome_ShouldNotBeEmpty()
-    {
+    public void Unwrap_WithOnlySome_ShouldNotBeEmpty() =>
         new List<Option<int>> { Option<int>.Some(3), Option<int>.Some(2) }
             .Unwrap()
             .Should()
             .NotBeEmpty().And
             .HaveCount(2);
-    }
 
     [Fact]
-    public void unwrap_WithMixed_ShouldHaveCorrectCount()
-    {
+    public void unwrap_WithMixed_ShouldHaveCorrectCount() =>
         new List<Option<string>>
             {
                 Option<string>.Some("test"),
@@ -32,5 +27,4 @@ public class OptionExtensionsTest
                 Option<string>.None
             }.Unwrap()
             .Should().NotBeEmpty().And.HaveCount(3);
-    }
 }
--- tests/OptionTest.cs
+++ tests/OptionTest.cs
@@ -6,73 +6,62 @@ namespace Sork.Funk.Tests;
 public class OptionTest
 {
     [Fact]
-    public void Some_Value_ReturnsIsSomeTrue()
-    {
-         Option<int>.Some(42).IsSome.Should().BeTrue();
-    }
+    public void Some_Value_ReturnsIsSomeTrue() => Option<int>.Some(42).IsSome.Should().BeTrue();
 
     [Fact]
-    public void None_ReturnsIsNoneTrue()
-    {
-        Option<int>.None.IsNone.Should().BeTrue();
-    }
+    public void None_ReturnsIsNoneTrue() => Option<int>.None.IsNone.Should().BeTrue();
 
     [Fact]
-    public void Map_SomeValue_ReturnsMappedValue()
-    {
+    public void Map_SomeValue_ReturnsMappedValue() =>
         Option<int>.Some(10)
             .Map(x => x * 2)
             .Match(some => some, () => -100)
             .Should().BePositive()
             .And.Be(20);
-    }
 
     [Fact]
-    public void Bind_SomeValue_ReturnsNewOption()
-    {
+    public void Bind_SomeValue_ReturnsNewOption() =>
         Option<int>.Some(5)
             .Bind(x => Option<string>.Some($"{x}"))
             .Match(some => some, () => "ALL WRONG")
             .Should().Be("5");
-    }
 
     [Fact]
-    public void Bind_None_ReturnsNone()
-    {
+    public void Bind_None_ReturnsNone() =>
         Option<int>.None.Map(x => x * 100)
             .IsNone
             .Should().BeTrue();
-    }
 
     [Fact]
     public void Some_WithNullInput_ShouldThrow()
     {
-        #nullable disable
-        Assert.Throws<NullReferenceException>( ()=> Option<string>.Some(null!).Map(x => x.Length));
-        Assert.Throws<InvalidOperationException>( ()=> Option<int?>.Some(null!).Map(x => x!.Value));
-        Assert.Throws<InvalidOperationException>( ()=> Option<bool?>.Some(null!).Map(x => x!.Value));
-        Assert.Throws<NullReferenceException>( ()=> Option<TestClass>.Some(null!).Map(x => x.GetType().Name));
-        Assert.Throws<InvalidOperationException>( ()=> Option<TestStruct?>.Some(null!).Map(x => x!.Value));
-        #nullable enable
+#nullable disable
+        Assert.Throws<NullReferenceException>(() => Option<string>.Some(null!).Map(x => x.Length));
+        Assert.Throws<InvalidOperationException>(() => Option<int?>.Some(null!).Map(x => x!.Value));
+        Assert.Throws<InvalidOperationException>(() => Option<bool?>.Some(null!).Map(x => x!.Value));
+        Assert.Throws<NullReferenceException>(() => Option<TestClass>.Some(null!).Map(x => x.GetType().Name));
+        Assert.Throws<InvalidOperationException>(() => Option<TestStruct?>.Some(null!).Map(x => x!.Value));
+#nullable enable
     }
 
     [Fact]
-    public void Some_WithString_ShouldEitherRight()
-    {
+    public void Some_WithString_ShouldEitherRight() =>
         Option<string>.Some("test")
             .ToEither(() => new Exception())
             .Match(l => l.Message, r => r)
             .Should().Be("test");
-    }
 
     [Fact]
-    public void Some_WithNone_ShouldEitherLeft()
-    {
+    public void Some_WithNone_ShouldEitherLeft() =>
         Option<int>.None.ToEither(() => new NullReferenceException())
             .Reduce(f => throw new AggregateException())
             .Should().BeOfType<NullReferenceException>();
+
+    private struct TestStruct
+    {
     }
-    
-    private struct TestStruct{}
-    private class TestClass{}
+
+    private class TestClass
+    {
+    }
 }
