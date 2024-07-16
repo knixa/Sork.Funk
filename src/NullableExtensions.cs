30d18627819cb2739f0bec1bb7292b7d75aa1f66
knixa <44531198+knixa@users.noreply.github.com>
2024-07-16 20:12:10 +0200

    add toOption for null structs

---
+++ src/NullableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace Sork.Funk;
+
+public static class NullableExtensions
+{
+    /// <summary>
+    /// Converts a nullable value to an <see cref="Option{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The underlying value type.</typeparam>
+    /// <param name="val">The nullable value.</param>
+    /// <returns>An <see cref="Option{T}"/> containing the value if it's not null; otherwise, <see cref="Option{T}.None"/>.</returns>
+    [Pure]
+    public static Option<T> ToOption<T>(this T? val) where T : struct =>
+        val.HasValue
+            ? Option<T>.Some(val.Value)
+            : Option<T>.None;
+}
---
+++ tests/NullableExtensionsTest.cs
@@ -0,0 +1,23 @@
+﻿using Sork.Funk;
+
+namespace Sork.Funk.Tests;
+
+public class NullableExtensionsTest
+{
+
+    [Fact]
+    public void ToOptions_WithNull_ShouldReturnNone()
+    {
+        int? val = null;
+
+        val.ToOption().IsNone.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ToOptions_WithValue_ShouldBeSome()
+    {
+        bool? val = true;
+
+        val.ToOption().IsSome.Should().BeTrue();
+    }
+}
