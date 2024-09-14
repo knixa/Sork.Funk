6d737d67e63a6b2e0fb9bbb6fb7e63a1cf07f34a
knixa <44531198+knixa@users.noreply.github.com>
2024-09-14 08:43:56 +0200

    add composition

---
+++ src/FunctionalComposition.cs
@@ -0,0 +1,9 @@
+﻿namespace Sork.Funk;
+
+public static partial class Composition
+{
+    public static Func<T, TResult> Compose<T, TIntermediate, TResult>(
+        this Func<T, TIntermediate> inner,
+        Func<TIntermediate, TResult> outer) =>
+        x => outer(inner(x));
+}
